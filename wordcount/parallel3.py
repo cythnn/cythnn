b29440a9a88bade30d7bd515d19984dd67d5d806
jeroenvuurens <jpbvuurens@gmail.com>
2016-03-28 21:06:06 +0200

    parallel3

--- wordcount/parallel3.py
+++ wordcount/parallel3.py
@@ -5,9 +5,9 @@ from tools.file import *
 from functools import partial
 
 @taketime("process")
-def process(file, cores = 2, parts = 2, rnge = None):
+def process(file, cores = 2, parts = 2, byterange = None):
     pool = Pool(processes=cores)
-    wordstreams = wordStreams(file, byterange=rnge)
+    wordstreams = wordStreams(file, byterange=byterange)
 
     tokens = pool.map(countWords, wordstreams)
     merged = mergeDicts(tokens)
@@ -16,6 +16,6 @@ def process(file, cores = 2, parts = 2, rnge = None):
     return terms
 
 if __name__ == '__main__':
-    terms = process("../text8", rnge=range(0,1000000), cores=2, parts=4)
+    terms = process("../text8", byterange=range(0, 1000000), cores=2, parts=4)
     for pair in terms[:20]:
         print(pair[0], ":", pair[1])
