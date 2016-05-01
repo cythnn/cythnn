046f6d167f25e13ae334637a74d12e62bced77f9
jeroenvuurens <jpbvuurens@gmail.com>
2016-05-01 14:18:22 +0200

    nqe

--- nqe/findmax.pyx
+++ nqe/findmax.pyx
@@ -3,7 +3,7 @@ from tools.types cimport *
 from numpy cimport *
 from tools.blas cimport saxpy, scopy, snrm2
 
-def loadvectors(fname):
+def loadvectors(fname, length=None):
     with open(fname, "r") as fin:
         header = fin.readline()
         vocab_size, vector_size = map(int, header.split())
@@ -21,8 +21,7 @@ def loadvectors(fname):
             vec2word[word_id] = word
 
         binary_len = dtype(float32).itemsize * vector_size
-        #for line_no in range(vocab_size):
-        for line_no in range(100):
+        for line_no in range(length if length is not None else vocab_size):
             word = []
             while True:
                 ch = fin.read(1)
@@ -57,8 +56,9 @@ def loadAnalogy(fname, word2vec):
 cdef float fmONE = -1.0
 cdef int iONE = 1
 
-cdef cREAL findmax(ndarray llines, ndarray ttable, int vectorsize, int vocsize):
+cdef cREAL findmax(ndarray llines, ndarray ttable):
     cdef int v, w, i, length = len(llines)
+    cdef int vectorsize = ttable.shape[1]
     cdef cREAL vlength
     cdef cREAL maxlength = 0
     cdef cINT *lines = toIArray(llines)
@@ -68,13 +68,80 @@ cdef cREAL findmax(ndarray llines, ndarray ttable, int vectorsize, int vocsize):
     for i in range(0, length, 2):
         v = lines[i]
         w = lines[i + 1]
-        memset(vec, 0, vectorsize * 4)
         scopy(&vectorsize, &table[v * vectorsize], &iONE, vec, &iONE)
         saxpy(&vectorsize, &fmONE, &table[w * vectorsize], &iONE, vec, &iONE)
         vlength = snrm2(&vectorsize, vec, &iONE)
         if vlength > maxlength:
             maxlength = vlength
         break
+    free(vec)
     return maxlength
+
+
+cdef void write(object file, ndarray resultv, ndarray resultn, ndarray results, int length):
+    for i in range(length):
+        pair=' '.join("%d" % val for val in results[i])
+        vector =' '.join("%f" % val for val in resultv[i])
+        file.write("%s %f %s\n" % (pair, resultn[i], vector))
+
+cdef void cluster(ndarray ttable, ndarray vtable, ndarray ntable, ndarray itable, int partition, int partitions, float maxlength):
+    cdef int m0, m1, v, w, i, rc = 0
+    cdef float mv0, mv1, a0, a1
+    cdef int length = ttable.shape[0]
+    cdef int vectorsize = ttable.shape[1]
+    cdef cREAL *table = toRArray(ttable)
+    cdef cREAL *vec = allocR(vectorsize)
+    cdef cINT *results = toIArray(itable)
+    cdef cREAL *resultv = toRArray(vtable)
+    cdef cREAL *resultn = toRArray(ntable)
+
+    cdef object file = open("out.%d"%partition, "w")
+
+    for v in range(1, length):
+        for w in range(v):
+            if (v + w % partitions == partition):
+                scopy( & vectorsize, & table[v * vectorsize], & iONE, vec, & iONE)
+                saxpy( & vectorsize, & fmONE, & table[w * vectorsize], & iONE, vec, & iONE)
+                vlength = snrm2( & vectorsize, vec, & iONE)
+                if vlength < maxlength:
+                    a0 = abs(vec[0])
+                    a1 = abs(vec[1])
+                    if a0 > a1:
+                        mv0 = a0
+                        m0 = 0
+                        mv1 = a1
+                        m1 = 1
+                    else:
+                        mv0 = a1
+                        m0 = 1
+                        mv1 = a0
+                        m1 = 0
+                    for i in range(2, vectorsize):
+                        a0 = abs(vec[i])
+                        if a0 > mv1:
+                            if a0 > mv0:
+                                mv1 = mv0
+                                m1 = m0
+                                mv0 = a0
+                                m0 = i
+                            else:
+                                mv1 = a0
+                                m1 = i
+                    results[rc * 4] = v
+                    results[rc * 4 + 1] = w
+                    results[rc * 4 + 2] = m0
+                    results[rc * 4 + 3] = m1
+                    resultn[rc] = vlength
+                    scopy( &vectorsize, vec, &iONE, &resultv[rc * vectorsize], &iONE)
+                    rc = rc + 1
+                    if (rc >= 10000):
+                        if True:#with gil:
+                           write(file, vtable, ntable, itable, rc)
+                        rc = 0
+    write(file, vtable, ntable, itable, rc)
+    file.close()
+
+
+
 
 
---
+++ train_skipgramHScached.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+from arch.SkipgramHS import SkipgramHS
+from model.model import Model
+from pipe.ContextWindows import contextWindow
+from pipe.ConvertWordIds import convertWordIds
+from pipe.DownSample import DownSample
+from tools.taketime import taketime
+from tools.word2vec import save
+from tools.worddict import buildvocab
+from pipe.createInputTasks import createW2VInputTasks
+
+def configure(inputrange=None):
+    return Model(alpha=0.025, vectorsize=100,
+                 input="data/text8",
+                 inputrange=inputrange,
+                 build=[ buildvocab ],
+                 pipeline=[ createW2VInputTasks, convertWordIds, DownSample, contextWindow, SkipgramHS ],
+                 mintf=5, cores=2, threads=2, windowsize=5, iterations=1, downsample=0.001, updatecacherate=0, cacheinner=0
+                 )
+
+@taketime("run")
+def time(m):
+    m.run()
+
+if __name__ == "__main__":
+    m = configure()
+    time(m)
+    save("results/vectors.sghs.bin", m, binary=True)
+    print("done", str(datetime.now()))
+
